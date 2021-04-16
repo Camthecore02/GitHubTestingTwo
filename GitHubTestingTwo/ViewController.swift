b5389967c2883c7ec374b6b3bdb5d08153eabec8
Cameron Smith <smith.cam1278@yahoo.com>
2021-04-16 12:39:26 -0400

    Initial Commit

---
+++ GitHubTestingTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTestingTwo
+//
+//  Created by Cameron Smith on 4/16/21.
+//  Copyright © 2021 Cameron Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
